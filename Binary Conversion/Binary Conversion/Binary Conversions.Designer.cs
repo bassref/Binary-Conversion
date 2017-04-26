ba72d651da4abede796e7f65a3fd56bb2b2176e3
bassref <rephie0000@gmail.com>
2017-04-26 11:51:48 -0500

    Add files via upload

---
+++ Binary Conversion/Binary Conversion/Binary Conversions.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Binary_Conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a Binary Number";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(42, 39);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(151, 20);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputBox.Click += new System.EventHandler(this.inputBox_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox1.Controls.Add(this.errorBox);
+            this.groupBox1.Controls.Add(this.submitButton);
+            this.groupBox1.Controls.Add(this.outputBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.inputBox);
+            this.groupBox1.Location = new System.Drawing.Point(67, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 238);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(42, 101);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(151, 23);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Click to Convert";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(62, 183);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(111, 22);
+            this.outputBox.TabIndex = 3;
+            this.outputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Decimal Conversion";
+            // 
+            // errorBox
+            // 
+            this.errorBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorBox.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.errorBox.Location = new System.Drawing.Point(42, 66);
+            this.errorBox.Name = "errorBox";
+            this.errorBox.ReadOnly = true;
+            this.errorBox.Size = new System.Drawing.Size(150, 22);
+            this.errorBox.TabIndex = 5;
+            this.errorBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 296);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Binary Conversions";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox errorBox;
+    }
+}
+
---
+++ Binary Conversion/Binary Conversion/Binary Conversions.cs
@@ -0,0 +1,107 @@
+﻿/* Name: Rephael Edwards
+ * Date: September 15th. 2016
+ * Professor: Dr. Stringfellow
+ * Program Description: This program allows a user to enter a binary number 
+ *                      and converst the number to a decimal.
+ 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Binary_Conversion
+{
+    public partial class Form1 : Form
+    {
+        //Precon: Form is created.
+        //Postcon:The program is executed.
+        //Purpose: To initialize the components of the form.
+        public Form1()
+        {
+            MessageBox.Show("Welcome to Binary Conversions \n");
+           
+            InitializeComponent();
+        }
+
+        //Precon: The convert button is clicked.
+        //Postcon:The function to activate the input box is called.
+        //Purpose: To call the input box function.
+        private void convert_Click(object sender, EventArgs e)
+        {            
+            inputBox_Click(sender, e);
+        }
+
+        //Precon: The convert button is clicked.
+        //Postcon:The number is converted.
+        //Purpose:To convert a binary number to a decimal.
+        private void inputBox_Click(object sender, EventArgs e)
+        {
+            //variables
+            int i = 0;
+            int sum = 0;
+            int ans = 0, dig;
+            var digits = new List<int>();
+            int[] numArray;
+            int length, textLength;
+
+            // Read in the number that was typed in the text box
+            string entry = inputBox.Text;           
+            //the value to compare the input to this
+            var regex = new Regex ("^[01]+$");
+
+            //if all the digits entered are either 0 or 1
+            if (BinaryCheck(entry, "^[01]+$"))
+            {
+                //clear the message in the error box
+                errorBox.Text = String.Empty;
+               
+                long num = Convert.ToInt64(inputBox.Text);
+                textLength = inputBox.TextLength;
+
+                // add each digit to a list
+                for (int n = 1; n <= textLength; n++)
+                {
+                    digits.Add((int)num % 10);
+                    num = num / 10;
+                }
+
+                // put the list in an array
+                numArray = digits.ToArray();
+                //get the length of the array
+                length = numArray.Length;
+
+                // for each digit, convert it to decimal and add it to the sum
+                for (i = 0; i < length; i++)
+                {
+                    dig = numArray[i];
+                    ans = (int)(dig * (Math.Pow(2, i)));
+                    sum += ans;
+                }
+                // output the answer to the UI
+                outputBox.Text = sum.ToString();
+            }
+            else
+            {
+                outputBox.Text = String.Empty;
+                errorBox.Text = "Invalid Entry";
+            }
+        }
+
+        //Precon: none
+        //Postcon:The number entered is checked for invalid entries
+        //Purpose:To check the value entered for invalid characters.
+        public Boolean BinaryCheck(string text, string regex)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(text, regex,
+                RegexOptions.IgnoreCase);
+        }
+    }      
+}
+
